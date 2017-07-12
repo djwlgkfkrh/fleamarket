3a9244c88442dcb2a98a1e2c3ca0f525448e9db4
djwlgkfkrh <djwlgkfkrh@users.noreply.github.com>
2017-07-12 16:59:37 +0900

    7.12 멤버VO만드는중

---
+++ FleaMarket/src/main/java/org/flea/domain/MemberVO.java
@@ -0,0 +1,12 @@
+package org.flea.domain;
+
+public class MemberVO {
+	private Integer ukey;
+	private String id;
+	private String pw;
+	private String name;
+	private Integer point;
+	private String nick;
+	private Integer cheater;
+	private Integer admin;
+}
