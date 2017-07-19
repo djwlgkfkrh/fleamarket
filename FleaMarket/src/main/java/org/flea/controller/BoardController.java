e2ceeda08ccbaa7f36b56fb01b6ccaaf4bbaae45
soojeong1992 <jscom_@naver.com>
2017-07-19 17:27:07 +0900

    soo commit

--- FleaMarket/src/main/java/org/flea/controller/BoardController.java
+++ FleaMarket/src/main/java/org/flea/controller/BoardController.java
@@ -19,76 +19,75 @@ import org.flea.service.BoardService;
 //@RequestMapping("/main")
 public class BoardController {
 	
-/*	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
 
 	@Inject
 	private BoardService service;
 	
 	
 	
-	 read Main / Sale / Buy start ======================================= 
-
+	/* read Main / Sale / Buy start ============= */
 	
-	// readMain : 메인에서 글읽기 
+	// readMain : 
 	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
 	public String readMain(Model model, UserVO vo, HttpServletRequest request) throws Exception {
 
-		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
-		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴
+		HttpSession session = request.getSession(); 
+		UserVO usersession = (UserVO) session.getAttribute("userinfo"); 
 		
-		System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의 Id 확인
+		System.out.println("id check : " + usersession.getId());
 
 		
 		logger.info("main post ...........");
-		//model.addAttribute(service.read(vo); // 모델로 하면 새로고침하면 사라짐 
+		//model.addAttribute(service.read(vo);
 
 		return "main";
 	}
 	
-	// readSale : 팝니다 에서 글읽기 
+	// readSale 
 	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
 	public String readSale(Model model, UserVO vo, HttpServletRequest request) throws Exception {
 
-		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
-		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴
+		HttpSession session = request.getSession(); 
+		UserVO usersession = (UserVO) session.getAttribute("userinfo");
 		
-		System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의 Id 확인
+		System.out.println("id check : " + usersession.getId()); 
 		
 		logger.info("main post ...........");
-		//model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로 하면 새로고침하면 사라짐 
+		//model.addAttribute("list", service.read(useridx.getUserkey())); 
 
 		return "main";
 	}
 	
-	// readBuy : 삽니다 에서 글읽기 
+	// readBuy 
 	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
 	public String readMBuy(Model model, UserVO vo, HttpServletRequest request) throws Exception {
 
-		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
-		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴
+		HttpSession session = request.getSession(); 
+		UserVO usersession = (UserVO) session.getAttribute("userinfo"); 
 		
-		System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의 Id 확인
+		System.out.println("id check : " + usersession.getId()); 
 		
 		logger.info("main post ...........");
-		//model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로 하면 새로고침하면 사라짐 
-
+		//model.addAttribute("list", service.read(useridx.getUserkey()));
+		
 		return "main";
 	}
 	
-	 read Main / Sale / Buy end ======================================= 
+
+	// read Main / Sale / Buy end ======================================= 
+	
+
 	
 	
-	
-		
-	
-	 Write => Sale / Buy Start ==========================================  
-	
-	
-	// createGET : 세션정보로 글 작성 세션GET
-	@RequestMapping(value = "/post", method = { RequestMethod.GET }) // 세션으로 글 작성 세션GET
+	// Write => Sale / Buy Start ==========================================  
+	// createGET 
+	@RequestMapping(value = "/post", method = { RequestMethod.GET }) 
 	public String createGET(BoardVO vo, HttpServletRequest request) throws Exception {
 
-		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
+		HttpSession session = request.getSession(); 
 		UserVO usersession = (UserVO) session.getAttribute("userinfo");
 		vo.setUserkey(usersession.getUserkey());
 
@@ -98,11 +97,11 @@ public class BoardController {
 	}
 	
 
-	// createPOST : 세션정보로 글 작성 세션 POST
-	@RequestMapping(value = "/post", method = { RequestMethod.POST }) // 가져온 세션으로 글 작성 POST
+	// createPOST 
+	@RequestMapping(value = "/post", method = { RequestMethod.POST })
 	public String createPOST(HttpServletRequest request,BoardVO vo) throws Exception {
 		
-		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
+		HttpSession session = request.getSession(); 
 		UserVO usersession = (UserVO) session.getAttribute("userinfo");
 		vo.setUserkey(usersession.getUserkey());
 		
@@ -118,29 +117,24 @@ public class BoardController {
 	}
 	
 	
-	
-	 Write => Sale / Buy end==========================================  
-	*/
-	
-/*	@RequestMapping(value = "/setting", method = RequestMethod.GET) // 세팅버튼 누르면 세션화면으로 가는부분
+	/*
+	@RequestMapping(value = "/setting", method = RequestMethod.GET)
 	public String settingGET(MemberVO vo, HttpServletRequest request) throws Exception {
 
-		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
+		HttpSession session = request.getSession(); 
 		MemberVO useridx = (MemberVO) session.getAttribute("userinfo");
-		vo.setUserid(useridx.getUserid()); // 여기는 userkey대신 id로.. 
+		vo.setUserid(useridx.getUserid());  
 
 		logger.info("userinfo GET ...........");
 
 		return "setting";
-	}*/
+	}
 	
 	
 	
 	
-	/// delete 미완 
-/*	
-		delete 버튼도 없이... 
-	@RequestMapping(value = "/deletePage", method = RequestMethod.POST) // 
+	/// delete
+	@RequestMapping(value = "/deletePage", method = RequestMethod.POST) 
 	  public String delete(@RequestParam("boardkey") int boardkey, BoardVO vo, RedirectAttributes rttr) throws Exception {
 
 	    service.delete(vo);
@@ -149,6 +143,6 @@ public class BoardController {
 
 	    return "redirect:/main";
 	  }
-	*/
-
+	
+*/
 }
