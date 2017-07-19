333d95866f30e9c47485f8923459eeb048492d73
soojeong1992 <jscom_@naver.com>
2017-07-19 16:15:23 +0900

    Board 하는중... 헷갈림

--- FleaMarket/src/main/java/org/flea/controller/BoardController.java
+++ FleaMarket/src/main/java/org/flea/controller/BoardController.java
@@ -23,47 +23,90 @@ public class BoardController {
 
 	@Inject
 	private BoardService service;
+	
+	
+	
+	
+	/* 글 읽는 부분  => 메인 / 팝니다 / 삽니다  시작 ======================================= */
 
 	
-	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
 	
-	public String read(Model model, UserVO vo, HttpServletRequest request) throws Exception {
+	// readMain : 메인에서 글읽기 
+	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
+	public String readMain(Model model, UserVO vo, HttpServletRequest request) throws Exception {
 
 		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
-		UserVO useridx = (UserVO) session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴
+		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴
+		
+		System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의 Id 확인
 
-		System.out.println("id check : " + useridx.getId()); // 옼 ! 나와따 ㅎㅎ
-
+		
 		logger.info("main post ...........");
-
-		//model.addAttribute("list", service.read(useridx.getUserkey()));
+		//model.addAttribute(service.read(vo); // 모델로 하면 새로고침하면 사라짐 
 
 		return "main";
 	}
 	
-	
+	// readSale : 팝니다 에서 글읽기 
+	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
+	public String readSale(Model model, UserVO vo, HttpServletRequest request) throws Exception {
 
+		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
+		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴
+		
+		System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의 Id 확인
+		
+		logger.info("main post ...........");
+		//model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로 하면 새로고침하면 사라짐 
+
+		return "main";
+	}
+	
+	// readBuy : 삽니다 에서 글읽기 
+	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
+	public String readMBuy(Model model, UserVO vo, HttpServletRequest request) throws Exception {
+
+		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
+		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴
+		
+		System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의 Id 확인
+		
+		logger.info("main post ...........");
+		//model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로 하면 새로고침하면 사라짐 
+
+		return "main";
+	}
+	
+	/* 글 읽는 부분  => 메인 / 팝니다 / 삽니다  끝 ======================================= */
+	
+	
+	
+		
+	
+	/* 글 쓰는 부분 => 팝니다 / 삽니다   시작  ==========================================  */
+	
+	
+	// createGET : 세션정보로 글 작성 세션GET
 	@RequestMapping(value = "/post", method = { RequestMethod.GET }) // 세션으로 글 작성 세션GET
 	public String createGET(BoardVO vo, HttpServletRequest request) throws Exception {
 
 		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
-		UserVO useridx = (UserVO) session.getAttribute("userinfo");
-		vo.setUserkey(useridx.getUserkey());
+		UserVO usersession = (UserVO) session.getAttribute("userinfo");
+		vo.setUserkey(usersession.getUserkey());
 
 		logger.info("postGET ...........");
 
 		return "post";
 	}
 	
-	
-	
 
+	// createPOST : 세션정보로 글 작성 세션 POST
 	@RequestMapping(value = "/post", method = { RequestMethod.POST }) // 가져온 세션으로 글 작성 POST
 	public String createPOST(HttpServletRequest request,BoardVO vo) throws Exception {
 		
 		HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
-		UserVO useridx = (UserVO) session.getAttribute("userinfo");
-		vo.setUserkey(useridx.getUserkey());
+		UserVO usersession = (UserVO) session.getAttribute("userinfo");
+		vo.setUserkey(usersession.getUserkey());
 		
 		// ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
 
@@ -75,6 +118,10 @@ public class BoardController {
 
 		return "redirect:/main";
 	}
+	
+	
+	
+	/* 글 쓰는 부분 => 팝니다 / 삽니다   끝  ==========================================  */
 	
 	
 /*	@RequestMapping(value = "/setting", method = RequestMethod.GET) // 세팅버튼 누르면 세션화면으로 가는부분
