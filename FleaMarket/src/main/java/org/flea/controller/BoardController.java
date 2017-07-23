7557169f223f564c5ac489da2cbd1979bf688a7a
djwlgkfkrh <djwlgkfkrh@users.noreply.github.com>
2017-07-23 13:56:19 +0900

    7/23 2:00 hgh + yj

--- FleaMarket/src/main/java/org/flea/controller/BoardController.java
+++ FleaMarket/src/main/java/org/flea/controller/BoardController.java
@@ -2,8 +2,11 @@ package org.flea.controller;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
+import org.flea.domain.BoardVO;
+import org.flea.domain.PageMaker;
+import org.flea.domain.SearchCriteria;
+import org.flea.service.BoardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,108 +15,58 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.flea.domain.PageMaker;
-import org.flea.domain.SearchCriteria;
-import org.flea.domain.BoardVO;
-import org.flea.domain.UserVO;
-import org.flea.service.BoardService;
 
 @Controller
 @RequestMapping("/sboard/*")
 public class BoardController {
 
-/*	
-
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Inject
 	private BoardService service;
-	
-
-
-	// readMain : ���ο��� ���б� 
-	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
-	public String readMain(Model model, UserVO vo, HttpServletRequest request) throws Exception {
-
-
-		HttpSession session = request.getSession(); // ���� �����ϰ� �������� �κ�
-		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session���� ""�� �ش��ϴ� ������ ������
-		
-		System.out.println("id check : " + usersession.getId()); // �������� ���� User�� Id Ȯ��
-
-		
-		logger.info("main post ...........");
-		//model.addAttribute(service.read(vo); // �𵨷� �ϸ� ���ΰ�ħ�ϸ� ����� 
-
-		return "main";
-	}
-
-	
-	// readSale : �˴ϴ� ���� ���б� 
-	@RequestMapping(value = "", method = { RequestMethod.GET, RequestMethod.POST })
-	public String readSale(Model model, UserVO vo, HttpServletRequest request) throws Exception {
 
 	@RequestMapping(value = "/list", method = { RequestMethod.POST, RequestMethod.GET })
 	public void salelist(@ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
 
-
-		HttpSession session = request.getSession(); // ���� �����ϰ� �������� �κ�
-		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session���� ""�� �ش��ϴ� ������ ������
-		
-		System.out.println("id check : " + usersession.getId()); // �������� ���� User�� Id Ȯ��
-		
-		logger.info("main post ...........");
-		//model.addAttribute("list", service.read(useridx.getUserkey())); // �𵨷� �ϸ� ���ΰ�ħ�ϸ� ����� 
-
-		return "main";
-	}
-	
-
-	// readBuy : ��ϴ� ���� ���б� 
-	@RequestMapping(value = "/salelist", method = { RequestMethod.GET, RequestMethod.POST })
-	public void salelist(Model model, UserVO vo, HttpServletRequest request) throws Exception {
-
-
-		HttpSession session = request.getSession(); // ���� �����ϰ� �������� �κ�
-		UserVO usersession = (UserVO) session.getAttribute("userinfo"); // session���� ""�� �ش��ϴ� ������ ������
-		
-		System.out.println("id check : " + usersession.getId()); // �������� ���� User�� Id Ȯ��
-		
-
 		logger.info("salelist post ...........");
-		//model.addAttribute("list", service.read(useridx.getUserkey())); // �𵨷� �ϸ� ���ΰ�ħ�ϸ� ����� 
-		//model.addAttribute("list", service.show());
+		// model.addAttribute("list", service.show());
 
+		// HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
+		// UserVO usersession = (UserVO) session.getAttribute("userinfo");
+		// vo.setUserkey(usersession.getUserkey());
 		model.addAttribute("list", service.listSearchCriteria(cri));
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
 
-	    PageMaker pageMaker = new PageMaker();
-	    pageMaker.setCri(cri);
+		// pageMaker.setTotalCount(service.listCountCriteria(cri));
+		pageMaker.setTotalCount(service.listSearchCount(cri));
 
-	    // pageMaker.setTotalCount(service.listCountCriteria(cri));
-	    pageMaker.setTotalCount(service.listSearchCount(cri));
-
-	    model.addAttribute("pageMaker", pageMaker);
+		model.addAttribute("pageMaker", pageMaker);
 
 	}
-	
-	 �� �д� �κ�  => ���� / �˴ϴ� / ��ϴ�  �� ======================================= 
-	
-	
-	
-		
-	
 
-	/* �� ���� �κ� => �˴ϴ� / ��ϴ�   ����  ==========================================  
-
-	
-	
-	// createGET : ���������� �� �ۼ� ����GET
-	@RequestMapping(value = "/post", method = { RequestMethod.GET }) // �������� �� �ۼ� ����GET
-	public String createGET(BoardVO vo, HttpServletRequest request) throws Exception {
-
-		HttpSession session = request.getSession(); // ���� �����ϰ� �������� �κ�
-		UserVO usersession = (UserVO) session.getAttribute("userinfo");
-		vo.setUserkey(usersession.getUserkey());
+	/*
+	 * 글 읽는 부분 => 메인 / 팝니다 / 삽니다 시작 ======================================= - -
+	 * - - - // readSale : 팝니다 에서 글읽기 - @RequestMapping(value = "", method = {
+	 * RequestMethod.GET, RequestMethod.POST }) - public String readSale(Model
+	 * model, UserVO vo, HttpServletRequest request) throws Exception { - -
+	 * HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분 - UserVO
+	 * usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에
+	 * 해당하는 세션을 가져옴 - - System.out.println("id check : " + usersession.getId());
+	 * // 세션으로 현재 User의 Id 확인 - - logger.info("main post ..........."); -
+	 * //model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로
+	 * 하면 새로고침하면 사라짐 - - return "main"; - } - - // readBuy : 삽니다 에서 글읽기
+	 * - @RequestMapping(value = "/salelist", method = { RequestMethod.GET,
+	 * RequestMethod.POST }) - public void salelist(Model model, UserVO vo,
+	 * HttpServletRequest request) throws Exception { - - HttpSession session =
+	 * request.getSession(); // 세션 선언하고 가져오는 부분 - UserVO usersession = (UserVO)
+	 * session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴 - -
+	 * System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의
+	 * Id 확인 - - logger.info("salelist post ..........."); -
+	 * //model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로
+	 * 하면 새로고침하면 사라짐
+	 * 
+	 */
 
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
 	public void read(@RequestParam("boardkey") int boardkey, Model model) throws Exception {
@@ -121,65 +74,45 @@ public class BoardController {
 		model.addAttribute("boardinfo", boardinfo);
 	}
 
+	/*
+	 * 글 읽는 부분 => 메인 / 팝니다 / 삽니다 끝 =======================================
+	 */
+	/*
+	 * 글 쓰는 부분 => 팝니다 / 삽니다 시작 ========================================== - - -
+	 * // createGET : 세션정보로 글 작성 세션GET - - - - // createPOST : 세션정보로 글 작성 세션
+	 * POST - @RequestMapping(value = "/post", method = { RequestMethod.POST })
+	 * // 가져온 세션으로 글 작성 POST - public String createPOST(HttpServletRequest
+	 * request,BoardVO vo) throws Exception { - - HttpSession session =
+	 * request.getSession(); // 세션 선언하고 가져오는 부분 - UserVO usersession = (UserVO)
+	 * session.getAttribute("userinfo"); -
+	 * vo.setUserkey(usersession.getUserkey()); - - // ModelAndView mv = new
+	 * ModelAndView("redirect:/sample/openBoardList.do"); - -
+	 * logger.info(vo.toString()); - - service.create(vo); - -
+	 * logger.info("postPOST ..........."); - - return "redirect:/main"; +
+	 */
 	@RequestMapping(value = "/post", method = { RequestMethod.GET })
 	public void createGET(BoardVO vo, HttpServletRequest request) throws Exception {
 		logger.info("postGET ...........");
-
 	}
-	
-
-	// createPOST : ���������� �� �ۼ� ���� POST
-	@RequestMapping(value = "/post", method = { RequestMethod.POST }) // ������ �������� �� �ۼ� POST
-	public String createPOST(HttpServletRequest request,BoardVO vo) throws Exception {
-		
-		HttpSession session = request.getSession(); // ���� �����ϰ� �������� �κ�
-		UserVO usersession = (UserVO) session.getAttribute("userinfo");
-		vo.setUserkey(usersession.getUserkey());
-		
-		// ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
-
-		logger.info(vo.toString());
-		
-		service.create(vo);
-
-		logger.info("postPOST ...........");
-
-		return "redirect:/main";
-	}
-	
-	
-	
-	 �� ���� �κ� => �˴ϴ� / ��ϴ�   ��  ==========================================  
-	
-	
-	@RequestMapping(value = "/setting", method = RequestMethod.GET) // ���ù�ư ������ ����ȭ������ ���ºκ�
-	public String settingGET(MemberVO vo, HttpServletRequest request) throws Exception {
-
-		HttpSession session = request.getSession(); // ���� �����ϰ� �������� �κ�
-		MemberVO useridx = (MemberVO) session.getAttribute("userinfo");
-		vo.setUserid(useridx.getUserid()); // ����� userkey��� id��.. 
-
-		logger.info("userinfo GET ...........");
-
-		return "setting";
-	}
-	
-	
-	
-	
-	/// delete �̿� 
-	
-		delete ��ư�� ����... 
-	@RequestMapping(value = "/deletePage", method = RequestMethod.POST) // 
-	  public String delete(@RequestParam("boardkey") int boardkey, BoardVO vo, RedirectAttributes rttr) throws Exception {
-
-	    service.delete(vo);
-	    
-	    rttr.addFlashAttribute("msg", "SUCCESS");
-
-	    return "redirect:/main";
-	  }
-	
-*/
+	/*
+	 * 글 쓰는 부분 => 팝니다 / 삽니다 끝 ==========================================
+	 */
+	/*
+	 * @RequestMapping(value = "/setting", method = RequestMethod.GET) // 세팅버튼
+	 * 누르면 세션화면으로 가는부분 - public String settingGET(MemberVO vo,
+	 * HttpServletRequest request) throws Exception { - - HttpSession session =
+	 * request.getSession(); // 세션 선언하고 가져오는 부분 - MemberVO useridx = (MemberVO)
+	 * session.getAttribute("userinfo"); - vo.setUserid(useridx.getUserid()); //
+	 * 여기는 userkey대신 id로.. - - logger.info("userinfo GET ..........."); - -
+	 * return "setting"; - }
+	 */
+	/// delete 미완
+	/*
+	 * - delete 버튼도 없이... - @RequestMapping(value = "/deletePage", method =
+	 * RequestMethod.POST) // - public String delete(@RequestParam("boardkey")
+	 * int boardkey, BoardVO vo, RedirectAttributes rttr) throws Exception { - -
+	 * service.delete(vo); - - rttr.addFlashAttribute("msg", "SUCCESS"); - -
+	 * return "redirect:/main"; - } -
+	 */
 
 }
