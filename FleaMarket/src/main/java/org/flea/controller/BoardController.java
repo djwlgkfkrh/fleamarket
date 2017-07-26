d66785ec78c22ba51f80d15660508de2a42de307
soojeong1992 <jscom_@naver.com>
2017-07-26 11:14:12 +0900

    7/26 file controller 변경중

--- FleaMarket/src/main/java/org/flea/controller/BoardController.java
+++ FleaMarket/src/main/java/org/flea/controller/BoardController.java
@@ -2,6 +2,7 @@ package org.flea.controller;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.flea.domain.BoardVO;
@@ -19,9 +20,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import org.flea.controller.PostFile;
+import org.flea.controller.UploadFile;
+import org.flea.domain.FileVO;
+import org.flea.service.FileService;
 
 
-@SessionAttributes("userinfo") // MemberVO ����
+@SessionAttributes("userinfo") // MemberVO
 @Controller
 @RequestMapping("/sboard/*")
 public class BoardController {
@@ -38,7 +45,7 @@ public class BoardController {
 
 		logger.info("salelist post ...........");
 		// model.addAttribute("list", service.show());
-		// HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분
+		// HttpSession session = request.getSession(); // �꽭�뀡 �꽑�뼵�븯怨� 媛��졇�삤�뒗 遺�遺�
 		// UserVO usersession = (UserVO) session.getAttribute("userinfo");
 		// vo.setUserkey(usersession.getUserkey());
 		model.addAttribute("list", service.listSearchCriteria(cri));
@@ -50,29 +57,9 @@ public class BoardController {
 
 	}
 
-	/*
-	 * 글 읽는 부분 => 메인 / 팝니다 / 삽니다 시작 ======================================= - -
-	 * - - - // readSale : 팝니다 에서 글읽기 - @RequestMapping(value = "", method = {
-	 * RequestMethod.GET, RequestMethod.POST }) - public String readSale(Model
-	 * model, UserVO vo, HttpServletRequest request) throws Exception { - -
-	 * HttpSession session = request.getSession(); // 세션 선언하고 가져오는 부분 - UserVO
-	 * usersession = (UserVO) session.getAttribute("userinfo"); // session에서 ""에
-	 * 해당하는 세션을 가져옴 - - System.out.println("id check : " + usersession.getId());
-	 * // 세션으로 현재 User의 Id 확인 - - logger.info("main post ..........."); -
-	 * //model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로
-	 * 하면 새로고침하면 사라짐 - - return "main"; - } - - // readBuy : 삽니다 에서 글읽기
-	 * - @RequestMapping(value = "/salelist", method = { RequestMethod.GET,
-	 * RequestMethod.POST }) - public void salelist(Model model, UserVO vo,
-	 * HttpServletRequest request) throws Exception { - - HttpSession session =
-	 * request.getSession(); // 세션 선언하고 가져오는 부분 - UserVO usersession = (UserVO)
-	 * session.getAttribute("userinfo"); // session에서 ""에 해당하는 세션을 가져옴 - -
-	 * System.out.println("id check : " + usersession.getId()); // 세션으로 현재 User의
-	 * Id 확인 - - logger.info("salelist post ..........."); -
-	 * //model.addAttribute("list", service.read(useridx.getUserkey())); // 모델로
-	 * 하면 새로고침하면 사라짐
-	 * 
-	 */
 
+	
+	
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
 	public void read(@RequestParam("boardkey") int boardkey, Model model,HttpSession session,HttpServletRequest request) throws Exception {
 		BoardVO boardinfo = service.read(boardkey);
@@ -88,46 +75,16 @@ public class BoardController {
 
 	}
 
-	/*
-	 * 글 읽는 부분 => 메인 / 팝니다 / 삽니다 끝 =======================================
-	 */
-	/*
-	 * 글 쓰는 부분 => 팝니다 / 삽니다 시작 ========================================== - - -
-	 * // createGET : 세션정보로 글 작성 세션GET - - - - // createPOST : 세션정보로 글 작성 세션
-	 * POST - @RequestMapping(value = "/post", method = { RequestMethod.POST })
-	 * // 가져온 세션으로 글 작성 POST - public String createPOST(HttpServletRequest
-	 * request,BoardVO vo) throws Exception { - - HttpSession session =
-	 * request.getSession(); // 세션 선언하고 가져오는 부분 - UserVO usersession = (UserVO)
-	 * session.getAttribute("userinfo"); -
-	 * vo.setUserkey(usersession.getUserkey()); - - // ModelAndView mv = new
-	 * ModelAndView("redirect:/sample/openBoardList.do"); - -
-	 * logger.info(vo.toString()); - - service.create(vo); - -
-	 * logger.info("postPOST ..........."); - - return "redirect:/main"; +
-	 */
+	
+	
+	
 	@RequestMapping(value = "/post", method = { RequestMethod.GET })
 	public void createGET(BoardVO vo) throws Exception {
 		logger.info("postGET ...........");
 	}
-	/*
-	 * 글 쓰는 부분 => 팝니다 / 삽니다 끝 ==========================================
-	 */
-	/*
-	 * @RequestMapping(value = "/setting", method = RequestMethod.GET) // 세팅버튼
-	 * 누르면 세션화면으로 가는부분 - public String settingGET(MemberVO vo,
-	 * HttpServletRequest request) throws Exception { - - HttpSession session =
-	 * request.getSession(); // 세션 선언하고 가져오는 부분 - MemberVO useridx = (MemberVO)
-	 * session.getAttribute("userinfo"); - vo.setUserid(useridx.getUserid()); //
-	 * 여기는 userkey대신 id로.. - - logger.info("userinfo GET ..........."); - -
-	 * return "setting"; - }
-	 */
-	/// delete 미완
-	/*
-	 * - delete 버튼도 없이... - @RequestMapping(value = "/deletePage", method =
-	 * RequestMethod.POST) // - public String delete(@RequestParam("boardkey")
-	 * int boardkey, BoardVO vo, RedirectAttributes rttr) throws Exception { - -
-	 * service.delete(vo); - - rttr.addFlashAttribute("msg", "SUCCESS"); - -
-	 * return "redirect:/main"; - } -
-	 */
+	
+	
+	
 
 
 	@RequestMapping(value = "/beforeread", method = { RequestMethod.GET, RequestMethod.POST })
@@ -143,6 +100,62 @@ public class BoardController {
 		int b = service.after(boardkey);
 		return "redirect:/sboard/read?boardkey=" + b;
 	}
+	
+	
+	// File start
+	@Inject
+	FileService fileservice;
+
+	
+	UploadFile FileUP = new UploadFile();
+	PostFile filepost = new PostFile();
+
+	@RequestMapping("/uploadPage")
+	private String uploadView() {
+
+		logger.info(" =========FileVO 시작 =========");
+		return "upload";
+	}
+
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	private String upload(@RequestParam("File") MultipartFile multipartFile) throws Exception {
+
+		if (multipartFile.isEmpty()) {
+
+			logger.info("FileVO 상태 : ====   파일없음    =====");
+			return " ";
+
+		} else {
+
+			logger.info("FileVO  상태 : ====   파일있음    =====");
+			FileVO vo  = FileUP.GetFile(multipartFile);		
+			fileservice.saveFile(vo); // service로 전달
+
+			logger.info("=======File service.saveFile(vo) 실행 완료 =====");
+
+			return "redirect:/uploadComplete";
+		}
+	}
+
+
+	@RequestMapping("/uploadComplete")
+	private void PostImage(FileVO vo, HttpServletResponse res, Model model) throws Exception {
+
+
+		FileVO vo2 = fileservice.postFile(vo);
+		logger.info("FileVO 상태 : ======= service.postFile(vo) 완료 =========");
+		
+		byte[] bytes = filepost.readFile(vo2.getFname());
+		filepost.write(res, vo2.getFileData());
+		
+	}
+	
+	// File end
+	
+	
+	
+	
 
 }
 
